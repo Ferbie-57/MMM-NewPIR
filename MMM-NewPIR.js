2df57dd3ae9f86b40229bae2c38be4cb85571051
bugsounet <github@bugsnana.fr>
2019-12-11 08:25:35 +0100

    node_helper push / use switch case

--- MMM-NewPIR.js
+++ MMM-NewPIR.js
@@ -1,7 +1,7 @@
 /******************
-   MMM_NewPIR v2
-   Bugsounet
-*******************/
+*  MMM_NewPIR v2  *
+*  Bugsounet      *
+******************/
 
 Module.register("MMM-NewPIR", {
     defaults: {
@@ -16,90 +16,120 @@ Module.register("MMM-NewPIR", {
     start: function () {
       this.counter = 0
       this.interval = null
-      console.log("[NewPIR] is now started");
-      this.sendSocketNotification("INIT", this.config);
+      console.log("[NewPIR] is now started")
+      this.config = this.configAssignment({}, this.defaults, this.config)
+      this.sendSocketNotification("INIT", this.config)
     },
 
     socketNotificationReceived: function (notification, payload) {
-      if (notification === "RESET_COUNTER") this.resetCountdown()
-      if (notification === "USER_PRESENCE") {
-        this.sendNotification("USER_PRESENCE", payload);
-        if (this.config.EconomyMode) {
-          if (payload) this.Showing()
-          else this.Hiding()
-        }
+      switch(notification) {
+        case "RESET_COUNTER":
+          this.resetCountdown()
+          break
+        case "USER_PRESENCE":
+          this.sendNotification("USER_PRESENCE", payload)
+          if (this.config.EconomyMode) {
+            if (payload) this.Showing()
+            else this.Hiding()
+          }
+          break
       }
     },
 
     notificationReceived: function (notification, payload) {
-
-      if (notification === "DOM_OBJECTS_CREATED") {
-        this.resetCountdown();
-        this.sendSocketNotification("START");
-      }
-      if (notification === "USER_PRESENCE") {
-        if (payload == true) {
-          this.resetCountdown();
-          this.sendSocketNotification("WAKEUP");
-        } else {
-	  clearInterval(this.interval);
-          var counter = document.querySelector("#NEWPIR.counter")
-          counter.textContent = "00:00:00"
-          this.counter = 0
-          this.sendSocketNotification("TIMER_EXPIRED");
-        }
+      switch(notification) {
+        case "DOM_OBJECTS_CREATED":
+          this.resetCountdown()
+          this.sendSocketNotification("START")
+          break
+        case "USER_PRESENCE":
+          if (payload == true) {
+            this.resetCountdown()
+            this.sendSocketNotification("WAKEUP")
+          } else {
+	    clearInterval(this.interval)
+            var counter = document.querySelector("#NEWPIR.counter")
+            counter.textContent = "00:00:00"
+            this.counter = 0
+            this.sendSocketNotification("TIMER_EXPIRED")
+          }
+          break
       }
     },
 
     resetCountdown: function () {
-      var self = this;
-      clearInterval(this.interval);
-      this.counter = this.config.delay;
+      var self = this
+      clearInterval(this.interval)
+      this.counter = this.config.delay
 
       this.interval = setInterval(function () {
-        self.counter -= 1000;
+        self.counter -= 1000
         var counter = document.querySelector("#NEWPIR.counter")
-	counter.textContent = new Date(self.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0];
+	counter.textContent = new Date(self.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0]
 
         if (self.counter <= 0) {
-          self.sendSocketNotification("TIMER_EXPIRED");
-          clearInterval(self.interval);
+          self.sendSocketNotification("TIMER_EXPIRED")
+          clearInterval(self.interval)
         }
-      }, 1000);
+      }, 1000)
     },
 
     Hiding: function() {
       var self = this
       MM.getModules().enumerate(function(module) {
         module.hide(1000, {lockString: self.identifier})
-      });
-      this.sendNotification("NEWPIR_HIDING");
-      console.log("[NewPIR] Hide All modules");
+      })
+      this.sendNotification("NEWPIR_HIDING")
+      console.log("[NewPIR] Hide All modules")
     },
 
     Showing: function(payload) {
-      var self = this;
+      var self = this
       MM.getModules().enumerate(function(module) {
         module.show(1000, {lockString: self.identifier})
-      });
-      this.sendNotification("NEWPIR_SHOWING");
-      console.log("[NewPIR] Show All modules");
+      })
+      this.sendNotification("NEWPIR_SHOWING")
+      console.log("[NewPIR] Show All modules")
     },
 
     getDom: function () {
-      var wrapper = document.createElement("div");
-      wrapper.id = "NEWPIR";
+      var wrapper = document.createElement("div")
+      wrapper.id = "NEWPIR"
       if (!this.config.debug) wrapper.className = "hidden"
       wrapper.classList.add("counter")
       wrapper.textContent = "--:--:--"
-      return wrapper;
+      return wrapper
     },
 
     getStyles: function () {
-      return ["MMM-NewPIR.css"];
+      return ["MMM-NewPIR.css"]
     },
 
     getScripts: function () {
-      return ["moment.js"];
+      return ["moment.js"]
     },
+
+    configAssignment : function (result) {
+    var stack = Array.prototype.slice.call(arguments, 1)
+    var item
+    var key
+    while (stack.length) {
+      item = stack.shift()
+      for (key in item) {
+        if (item.hasOwnProperty(key)) {
+          if (typeof result[key] === "object" && result[key] && Object.prototype.toString.call(result[key]) !== "[object Array]" ) {
+              if (typeof item[key] === "object" && item[key] !== null) {
+                  result[key] = this.configAssignment({}, result[key], item[key])
+              } else {
+                result[key] = item[key]
+              }
+          } else {
+            result[key] = item[key]
+          }
+        }
+      }
+    }
+    return result
+  },
+
 });
--- node_helper.js
+++ node_helper.js
@@ -1,97 +1,109 @@
-const NodeHelper = require('node_helper');
-const exec = require('child_process').exec;
-const fs = require('fs');
-const Gpio = require('onoff').Gpio;
+/********************************
+* node_helper for MMM_NewPir v2 *
+* BuGsounet                     *
+********************************/
+
+const NodeHelper = require('node_helper')
+const exec = require('child_process').exec
+const fs = require('fs')
+const Gpio = require('onoff').Gpio
 
 module.exports = NodeHelper.create({
 
-	running: false,
+  start: function() {
+    console.log('Starting node_helper for module ' + this.name)
+    this.running = false
+    this.MyGovernor = [ "conservative", "ondemand" , "userspace" , "powersave" , "performance" ]
+    this.Governor = {
+      "actived" : false,
+      "governor" : ""
+    }
+  },
 
-    	socketNotificationReceived: function (notification, payload) {
-        	const self = this;
-		if (notification === "WAKEUP") {
-			self.running = true;
-			if (self.config.turnOffDisplay) {
-				exec("/usr/bin/vcgencmd display_power 1");
-			}
-			console.log("[NewPIR] Wake Up Detected")
-		}
+  initialize: function() {
+    console.log("[NewPIR] Initialize...")
+    this.WantedDisplay(true)
+    this.sendSocketNotification("USER_PRESENCE", true)
+    console.log("[NewPIR] Init Display...")
+    this.setGovernor()
+    console.log("[NewPIR] Sensor Active: pin " + this.config.sensorPin)
+    this.running = true
+    console.log("[NewPIR] Initialize Complete")
+  },
 
-		if (notification === "ASSIST") { // Be back For Assistant
-			self.running = true;
-                	self.sendSocketNotification("USER_PRESENCE", true); // Presence Force
-                	if (self.config.turnOffDisplay) {
-				exec("/usr/bin/vcgencmd display_power 1"); // and Force HDMI power on
-                	}
-                	console.log("[NewPIR] Wake Up Detected")
-        	};
+  socketNotificationReceived: function (notification, payload) {
+    switch(notification) {
+      case "INIT":
+        this.config = payload
+        this.initialize()
+        break
+      case "START":
+        this.pirStatus()
+        break
+      case "TIMER_EXPIRED":
+        this.running = false
+        this.sendSocketNotification("USER_PRESENCE", false)
+        if (this.config.turnOffDisplay) this.WantedDisplay(false)
+        break
+      case "WAKEUP":
+        this.running = true
+        this.sendSocketNotification("USER_PRESENCE", true)
+        if (this.config.turnOffDisplay) this.WantedDisplay(true)
+        if (this.config.debug) console.log("[NewPIR] Wake Up Detected")
+        break
+    }
+  },
 
-        	if (notification === "START") {
-	    		this.config = payload;
-            		if (!self.running) { // Init for First Start
-           			self.running = true;
-        			self.sendSocketNotification("USER_PRESENCE", true); // Presence Force
-				exec("/usr/bin/vcgencmd display_power 1"); //force HDMI ON
-       	       	 		console.log("[NewPIR] Init Display...")
-				// Init Display is ok, Let's Init Governor now !
-				// Governor : conservative ondemand userspace powersave performance
-				if ((this.config.Governor == "conservative") || (this.config.Governor == "ondemand") || (this.config.Governor == "userspace") || (this.config.Governor == "powersave") || (this.config.Governor == "performance")) {
-					exec("echo " + this.config.Governor + " | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
-					console.log("[NewPIR] Init Governor : " +  this.config.Governor);
-				} else {
-					if(this.config.Governor == "")
-						console.log("[NewPIR] Init Governor : Desactived.");
-					else
-						console.log("[NewPIR] Init Governor : Error ! Unknow Governor.");
-				}
-            		};
-			if (this.config.useSensor) {
-				console.log("[New-Pir] Sensor Active " + this.config.useSensor)
-				// Gpio sensor test pin
-            			this.pir = new Gpio(this.config.sensorPin, 'in', 'both');
-            			this.pir.watch(function (err, value) {
-					if (self.config.debug) {
-						console.log("[New-Pir] Sensor read value : " + value)
-					}
-            				if (value == 1) {
-						// Check HDMI power state
-						self.checkDisplay();
-						// send user presence for reset counter
-						self.sendSocketNotification("USER_PRESENCE", true);
-						setTimeout(()=>{ // timeout For Result display !?!?
-							if (self.config.debug) console.log("[NewPIR] DisplayResult : " + this.DisplayResult)
-							if (this.DisplayResult == "0" && self.config.turnOffDisplay) {
-								exec("/usr/bin/vcgencmd display_power 1");
-								console.log("[NewPIR] Display ON")
-							}
-						}, 600);
-                				if (!self.running) {
-                        				self.running = true;
-							self.sendSocketNotification("NEWPIR_SHOWING");
-                    				}
-                			}
-            			});
-			}
+  pirStatus: function() {
+    var self = this
+    this.pir = new Gpio(this.config.sensorPin, 'in', 'both')
+    this.pir.watch(function (err, value) {
+      if (this.config.debug) console.log("[NewPIR] Sensor read value : " + value)
+      if (value == 1) {
+        self.sendSocketNotification("RESET_COUNTER")
+        self.WantedDisplay(true)
+        if (!self.running) {
+          self.sendSocketNotification("USER_PRESENCE", true)
+          self.running = true
+        }
+      }
+    })
+  },
 
-        	} else if (notification === "TIMER_EXPIRED") {
-        		self.running = false;
-			self.sendSocketNotification("USER_PRESENCE", false);
-			self.sendSocketNotification("NEWPIR_HIDING");
-			if (self.config.turnOffDisplay) {
-				exec("/usr/bin/vcgencmd display_power 0");
-				console.log("[NewPIR] Display OFF")
-			}
-		}
-   	},
+  WantedDisplay: function(wanted) {
+    exec("/usr/bin/vcgencmd display_power", (err, stdout, stderr)=> {
+      if (err == null) {
+        var displaySh = stdout.trim()
+        var actual = Boolean(Number(displaySh.substr(displaySh.length -1)))
+        if (this.config.debug) console.log("[NewPIR] Display -- Actual : " + actual + " - Wanted : " + wanted)
+        if (actual && !wanted) this.setDisplay(false)
+        if (!actual && wanted) this.setDisplay(true)
+      }
+      else console.log("[NewPIR] Display Error")
+    })
+  },
 
-	// checkDisplay : DisplayResult return => 0 when HDMI power Off,  1 when HDMI power On
-	checkDisplay: function(power) {
-		exec("/usr/bin/vcgencmd display_power", (err, stdout, stderr)=> {
-			if (err == null) {
-				var displaySh = stdout.trim();
-				DisplayResult = displaySh.substr(displaySh.length -1);
-				//console.log("[NewPIR] Test -- Fonction checkDisplay: " + DisplayResult);
-			}
-		})
-	}
+  setDisplay: function(set) {
+    if (set)  exec("/usr/bin/vcgencmd display_power 1")
+    else exec("/usr/bin/vcgencmd display_power 0")
+    if (this.config.debug) console.log("[NewPIR] Display " + (set ? "ON" : "OFF"))
+  },
+
+  setGovernor: function() {
+    for (let [item, value] of Object.entries(this.MyGovernor)) {
+      if (value == this.config.Governor) {
+         this.Governor.actived = true
+         this.Governor.governor = value
+         exec("echo " + value + " | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
+         console.log("[NewPIR] Init Governor : " +  value)
+      }
+    }
+    if (this.config.Governor == "") {
+      this.Governor.actived = false
+      this.Governor.governor = "desactived"
+      console.log("[NewPIR] Init Governor : Desactived")
+    }
+    if (!this.Governor.actived && !(this.Governor.governor == "desactived")) console.log("[NewPIR] Init Governor Error : unknow Governor (" + this.config.Governor + ")")
+  },
+
 });
