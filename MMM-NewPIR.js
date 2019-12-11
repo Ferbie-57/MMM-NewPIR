edd67c29fb6ba3f42d73ded7fa5df80709d1fb27
bugsounet <github@bugsnana.fr>
2019-12-11 04:35:07 +0100

    V2

--- MMM-NewPIR.js
+++ MMM-NewPIR.js
@@ -1,111 +1,103 @@
-Module.register("MMM-NewPIR", {
+/******************
+   MMM_NewPIR v2
+   Bugsounet
+*******************/
 
+Module.register("MMM-NewPIR", {
     defaults: {
-	useSensor: true,
-        sensorPin: 21,
-        delay: 15 * 1000,
-        turnOffDisplay: true,
-	EconomyMode: true,
-	Governor: "",
-	debug: false
+      sensorPin: 21,
+      delay: 60 * 1000,
+      turnOffDisplay: true,
+      EconomyMode: true,
+      Governor: "",
+      debug: false
     },
 
     start: function () {
-        Log.log("[NewPIR] is started");
-	this.sendSocketNotification("START", this.config);
+      this.counter = 0
+      this.interval = null
+      console.log("[NewPIR] is now started");
+      this.sendSocketNotification("INIT", this.config);
     },
 
     socketNotificationReceived: function (notification, payload) {
-        if (notification === "USER_PRESENCE") {
-		if (payload) this.resetCountdown();
-		this.sendNotification('USER_PRESENCE', payload);
+      if (notification === "RESET_COUNTER") this.resetCountdown()
+      if (notification === "USER_PRESENCE") {
+        this.sendNotification("USER_PRESENCE", payload);
+        if (this.config.EconomyMode) {
+          if (payload) this.Showing()
+          else this.Hiding()
         }
-	if ((this.config.EconomyMode) && (notification === "NEWPIR_HIDING") {
-		this.Hiding();
-	}
-	if ((this.config.EconomyMode) && (notification === "NEWPIR_SHOWING") {
-		this.Showing();
-	}
+      }
     },
 
     notificationReceived: function (notification, payload) {
 
-        if (notification === 'DOM_OBJECTS_CREATED') {
-            //DOM creation complete, let's start the module
-            	this.resetCountdown();
+      if (notification === "DOM_OBJECTS_CREATED") {
+        this.resetCountdown();
+        this.sendSocketNotification("START");
+      }
+      if (notification === "USER_PRESENCE") {
+        if (payload == true) {
+          this.resetCountdown();
+          this.sendSocketNotification("WAKEUP");
+        } else {
+	  clearInterval(this.interval);
+          var counter = document.querySelector("#NEWPIR.counter")
+          counter.textContent = "00:00:00"
+          this.counter = 0
+          this.sendSocketNotification("TIMER_EXPIRED");
         }
-	if (notification === "USER_PRESENCE") {
-                if (payload) {
-                        this.resetCountdown();
-			this.sendSocketNotification("WAKEUP");
-		} else {
-			this.sendSocketNotification("TIMER_EXPIRED");
-		}
-	}
+      }
     },
 
     resetCountdown: function () {
-        var self = this;
-        clearInterval(self.interval);
-	self.counter = this.config.delay;
-	//for debug
-        self.updateDom();
+      var self = this;
+      clearInterval(this.interval);
+      this.counter = this.config.delay;
 
-        self.interval = setInterval(function () {
-            self.counter -= 1000;
-            if (self.counter <= 0) {
-                self.sendSocketNotification('TIMER_EXPIRED');
-		clearInterval(self.interval);
-            }
-	    //for debug
-	    self.updateDom();
-        }, 1000);
+      this.interval = setInterval(function () {
+        self.counter -= 1000;
+        var counter = document.querySelector("#NEWPIR.counter")
+	counter.textContent = new Date(self.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0];
+
+        if (self.counter <= 0) {
+          self.sendSocketNotification("TIMER_EXPIRED");
+          clearInterval(self.interval);
+        }
+      }, 1000);
     },
 
     Hiding: function() {
-	    var self = this;
-            MM.getModules().exceptModule(this).enumerate(function(module) {
-                module.hide(1000, null, {lockString: self.identifier})
-            });
-            console.log("[NewPIR] Hide All modules");
+      var self = this
+      MM.getModules().enumerate(function(module) {
+        module.hide(1000, {lockString: self.identifier})
+      });
+      console.log("[NewPIR] Hide All modules");
     },
 
     Showing: function(payload) {
-	    var self = this;
-            MM.getModules().exceptModule(this).enumerate(function(module) {
-               	module.show(1000, null, {lockString: self.identifier})
-            });
-            console.log("[NewPIR] Show All modules");
+      var self = this;
+      MM.getModules().enumerate(function(module) {
+        module.show(1000, {lockString: self.identifier})
+      });
+      console.log("[NewPIR] Show All modules");
     },
 
-// For debug
-
-
     getDom: function () {
-        var self = this;
-
-        var html = document.createElement("div");
-	html.id = "#NEWPIR";
-	if (this.config.debug) m.classList.remove("inactive")
-	else m.classList.add("inactive")
-        //html.className = "wrapper";
-
-        if (typeof self.counter !== "undefined") {
-        	var time = document.createElement("div");
-                time.className = "time";
-		time.innerText = new Date(this.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0];
-                html.appendChild(time);
-        }
-
-        return html;
+      var wrapper = document.createElement("div");
+      wrapper.id = "NEWPIR";
+      if (!this.config.debug) wrapper.classList.add("hidden")
+      wrapper.className = "counter"
+      wrapper.textContent = "--:--:--"
+      return wrapper;
     },
 
     getStyles: function () {
-        return ["mmm-newpir.css"];
+      return ["MMM-NewPIR.css"];
     },
 
     getScripts: function () {
-        return ["moment.js"];
+      return ["moment.js"];
     },
-
 });
