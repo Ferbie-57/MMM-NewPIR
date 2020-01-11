385a0ebbc0bc6f9f496034faac3862c0d1cd44b1
bugsounet <github@bugsnana.fr>
2020-01-11 10:07:06 +0100

    delete eventwakeup

--- MMM-NewPIR.js
+++ MMM-NewPIR.js
@@ -7,11 +7,10 @@ Module.register("MMM-NewPIR", {
     defaults: {
       useSensor: true,
       sensorPin: 21,
-      delay: 60 * 1000,
+      delay: 2* 60 * 1000,
       turnOffDisplay: true,
       ecoMode: true,
       governor: "",
-      eventWakeup: ["ASSISTANT_ACTIVATE" , "ASSISTANT_STANDBY"],
       debug: false
     },
 
@@ -57,32 +56,8 @@ Module.register("MMM-NewPIR", {
             this.sendSocketNotification("WAKEUP")
           } else ForceExpire()
           break
-        case "MODULE_DOM_CREATED":
-        case "ALL_MODULES_STARTED":
-        case "SHOW_ALERT":
-        case "SHOW_ALERT":
-        case "HIDE_ALERT":
-        case "CLOCK_SECOND":
-        case "CLOCK_MINUTE":
-        case "CALENDAR_EVENTS":
-        case "CURRENTWEATHER_DATA":
-        case "ADD_FEED":
-        case "NEWS_FEED_UPDATE":
-        case "NEWS_FEED":
-          // just ignore all standard notification
-          break
         default :
-          this.scanOtherNotification(notification)
           break
-      }
-    },
-
-    scanOtherNotification: function (notification) {
-      for (let [item, value] of Object.entries(this.config.eventWakeup)) {
-        if (value == notification) {
-		this.notificationReceived("USER_PRESENCE", true, this.name)
-		console.log("[NewPIR] Event Wakeup: " + notification)
-        }
       }
     },
 
@@ -94,7 +69,7 @@ Module.register("MMM-NewPIR", {
       this.interval = setInterval(function () {
         self.counter -= 1000
         var counter = document.querySelector("#NEWPIR.counter")
-	counter.textContent = new Date(self.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0]
+        counter.textContent = new Date(self.counter).toUTCString().match(/\d{2}:\d{2}:\d{2}/)[0]
 
         if (self.counter <= 0) {
           self.sendSocketNotification("TIMER_EXPIRED")
