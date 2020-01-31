b5dfcc0404f916885b88f1200b5ab37bae0431d3
bugsounet <github@bugsnana.fr>
2020-01-31 16:06:16 +0100

    update installer

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -2,6 +2,6 @@
 
 npm install electron-rebuild
 ./node_modules/.bin/electron-rebuild
-sudo usermod -a -G gpio pi
-sudo chmod u+s /opt/vc/bin/tvservice && sudo chmod u+s /bin/chvt
+sudo usermod -a -G gpio pi || echo "Error command: sudo usermod -a -G gpio pi"
+sudo chmod -f u+s /opt/vc/bin/tvservice && sudo chmod -f u+s /bin/chvt || echo "Error command: sudo chmod u+s /opt/vc/bin/tvservice && sudo chmod u+s /bin/chvt"
 
