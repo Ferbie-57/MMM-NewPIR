d10465dc37a0fcf4d65ca952a22a1c9d24ef9030
bugsounet <bugsounet@bugsnana.fr>
2020-07-28 04:07:24 +0200

    v3.0.0

--- MMM-NewPIR.js
+++ MMM-NewPIR.js
@@ -5,13 +5,28 @@
 
 Module.register("MMM-NewPIR", {
     defaults: {
-      sensorPin: 21,
-      delay: 2* 60 * 1000,
-      turnOffDisplay: true,
-      ecoMode: true,
-      text: "Auto Turn Off Screen:",
-      counter: true,
-      debug: false,
+      debug: true,
+      screen: {
+        delay: 2 * 60 * 1000,
+        turnOffDisplay: true,
+        ecoMode: true,
+        displayCounter: true,
+        text: "Auto Turn Off Screen:",
+        displayBar: true,
+        displayStyle: "Text",
+        governorSleeping: false,
+        rpi4: false
+      },
+      pir: {
+        usePir: true,
+        gpio: 21,
+        reverseValue: false
+      },
+      governor: {
+        useGovernor: false,
+        sleeping: "powersave",
+        working: "ondemand"
+      }
     },
 
     start: function () {
@@ -23,107 +38,110 @@ Module.register("MMM-NewPIR", {
       mylog = function() {
         //do nothing
       }
-      this.counter = 0
-      this.interval = null
+
       this.config = Object.assign({}, this.defaults, this.config)
-      this.helperConfig = {
-          "pin": this.config.sensorPin,
-          "display": this.config.turnOffDisplay,
-          "debug": this.config.debug
-      }
       if (this.config.debug) mylog = mylog_
-      this.sendSocketNotification("INIT", this.helperConfig)
+      this.sendSocketNotification("INIT", this.config)
       mylog("is now started!")
     },
 
     socketNotificationReceived: function (notification, payload) {
       switch(notification) {
-        case "RESET_COUNTER":
-          this.resetCountdown()
-          break
-        case "PRESENCE":
-          if (this.config.ecoMode) {
-            if (payload) this.Showing()
-            else this.Hiding()
-          }
-          this.sendNotification("USER_PRESENCE", payload)
-          break
+      case "SCREEN_SHOWING":
+        this.screenShowing()
+        break
+      case "SCREEN_HIDING":
+        this.screenHiding()
+        break
+      case "SCREEN_TIMER":
+        if (this.config.screen.displayStyle == "Text") {
+          let counter = document.getElementById("NEWPIR_SCREEN_COUNTER")
+          counter.textContent = payload
+        }
+        break
+      case "SCREEN_BAR":
+        if (this.config.screen.displayStyle == "Bar") {
+          let bar = document.getElementById("NEWPIR_SCREEN_BAR")
+          bar.value= this.config.screen.delay - payload
+        }
+        else if (this.config.screen.displayStyle != "Text") {
+          let value = (100 - ((payload * 100) / this.config.screen.delay))/100
+          let timeOut = moment(new Date(this.config.screen.delay-payload)).format("mm:ss")
+          this.bar.animate(value, {
+            step: (state, bar) => {
+              bar.path.setAttribute('stroke', state.color)
+              bar.setText(this.config.screen.displayCounter ? timeOut : "")
+              bar.text.style.color = state.color
+            }
+          })
+        }
+        break
+      case "SCREEN_PRESENCE":
+        this.sendNotification("USER_PRESENCE", payload ? true : false)
+        break
       }
     },
 
     notificationReceived: function (notification, payload) {
       switch(notification) {
         case "DOM_OBJECTS_CREATED":
-          this.resetCountdown()
-          this.sendSocketNotification("START")
+          this.prepareBar()
           break
         case "USER_PRESENCE":
           if (payload == true) {
-            this.resetCountdown()
             this.sendSocketNotification("WAKEUP")
-          } else this.ForceExpire()
+          }
+          else this.sendSocketNotification("FORCE_END")
           break
-        default :
+        case "SCREEN_END":
+          this.sendSocketNotification("FORCE_END")
+          break
+        case "SCREEN_WAKEUP":
+          this.sendSocketNotification("WAKEUP")
+          break
+        case "SCREEN_LOCK":
+          this.sendSocketNotification("LOCK")
+          break
+        case "SCREEN_UNLOCK":
+          this.sendSocketNotification("UNLOCK")
           break
       }
-    },
-
-    resetCountdown: function () {
-      clearInterval(this.interval)
-      this.interval = null
-      this.counter = this.config.delay
-
-      this.interval = setInterval( ()=> {
-        this.counter -= 1000
-        var counter = document.getElementById("NEWPIR_COUNTER")
-        counter.textContent = moment(new Date(this.counter)).format("mm:ss")
-
-        if (this.counter <= 0) {
-          this.sendSocketNotification("TIMER_EXPIRED")
-          clearInterval(this.interval)
-          this.interval = null
-        }
-      }, 1000)
-    },
-
-    ForceExpire: function(){
-      clearInterval(this.interval)
-      var counter = document.getElementById("NEWPIR_COUNTER")
-      counter.textContent = "00:00"
-      this.counter = 0
-      this.sendSocketNotification("TIMER_EXPIRED")
-    },
-
-    Hiding: function() {
-      MM.getModules().enumerate( (module)=> {
-        module.hide(1000, {lockString: this.identifier})
-      })
-      mylog("Hide All modules.")
-    },
-
-    Showing: function(payload) {
-      MM.getModules().enumerate( (module)=> {
-        module.show(1000, {lockString: this.identifier})
-      })
-      mylog("Show All modules.")
     },
 
     getDom: function () {
       var dom = document.createElement("div")
       dom.id = "NEWPIR"
-      if (!this.config.counter) dom.className = "hidden"
 
-      var t = document.createElement("div")
-      t.id = "NEWPIR_TEXT"
-      t.textContent = this.config.text
-      dom.appendChild(t)
+      if (this.config.screen.displayCounter || this.config.screen.displayBar) {
+        /** Screen TimeOut Text **/
+        var screen = document.createElement("div")
+        screen.id = "NEWPIR_SCREEN"
+        if (this.config.screen.displayStyle != "Text") screen.className = "hidden"
+        var screenText = document.createElement("div")
+        screenText.id = "NEWPIR_SCREEN_TEXT"
+        screenText.textContent = this.config.screen.text
+        screen.appendChild(screenText)
+        var screenCounter = document.createElement("div")
+        screenCounter.id = "NEWPIR_SCREEN_COUNTER"
+        screenCounter.classList.add("counter")
+        screenCounter.textContent = "--:--"
+        screen.appendChild(screenCounter)
 
-      var c = document.createElement("div")
-      c.id = "NEWPIR_COUNTER"
-      c.classList.add("counter")
-      c.textContent = "--:--"
-      dom.appendChild(c)
-
+        /** Screen TimeOut Bar **/
+        var bar = document.createElement("div")
+        bar.id = "NEWPIR_BAR"
+        if ((this.config.screen.displayStyle == "Text") || !this.config.screen.displayBar) bar.className = "hidden"
+        var screenBar = document.createElement(this.config.screen.displayStyle == "Bar" ? "meter" : "div")
+        screenBar.id = "NEWPIR_SCREEN_BAR"
+        screenBar.classList.add(this.config.screen.displayStyle)
+        if (this.config.screen.displayStyle == "Bar") {
+          screenBar.value = 0
+          screenBar.max= this.config.screen.delay
+        }
+        bar.appendChild(screenBar)
+        dom.appendChild(screen)
+        dom.appendChild(bar)
+      }
       return dom
     },
 
@@ -132,6 +150,50 @@ Module.register("MMM-NewPIR", {
     },
 
     getScripts: function () {
-      return ["moment.js"]
+      return [
+        "/modules/MMM-NewPIR/scripts/progressbar.js"
+      ]
     },
+
+    prepareBar: function () {
+      /** Prepare TimeOut Bar **/
+      if ((this.config.screen.displayStyle == "Text") || (this.config.screen.displayStyle == "Bar") || (!this.config.screen.displayBar)) return
+      this.bar = new ProgressBar[this.config.screen.displayStyle](document.getElementById('NEWPIR_SCREEN_BAR'), {
+        strokeWidth: this.config.screen.displayStyle == "Line" ? 2 : 5,
+        trailColor: '#1B1B1B',
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 500,
+        svgStyle: null,
+        from: {color: '#FF0000'},
+        to: {color: '#00FF00'},
+        text: {
+          style: {
+            position: 'absolute',
+            left: '50%',
+            top: this.config.screen.displayStyle == "Line" ? "0" : "50%",
+            padding: 0,
+            margin: 0,
+            transform: {
+                prefix: true,
+                value: 'translate(-50%, -50%)'
+            }
+          }
+        }
+      })
+    },
+
+    screenShowing: function() {
+      MM.getModules().enumerate((module)=> {
+        module.show(1000, {lockString: "NEWPIR_LOCK"})
+      })
+      mylog("Show All modules.")
+    },
+
+    screenHiding: function() {
+      MM.getModules().enumerate((module)=> {
+        module.hide(1000, {lockString: "NEWPIR_LOCK"})
+      })
+      mylog("Hide All modules.")
+    }
 });
--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,9 @@
 ********************************/
 
 const NodeHelper = require('node_helper')
-const exec = require('child_process').exec
-const process = require('process')
+const Screen = require("@bugsounet/screen")
 const Pir = require("@bugsounet/pir")
+const Governor = require("@bugsounet/governor")
 
 var _log = function() {
     var context = "[NewPIR]"
@@ -21,93 +21,65 @@ module.exports = NodeHelper.create({
 
   start: function() {
     console.log('Starting node_helper for module ' + this.name)
-    this.running = false
-    this.pirConf = {
-      "gpio": 21
-    }
   },
 
   initialize: function() {
     console.log("[NewPIR] Initialize...")
-    var debug = (this.conf.debug) ? this.conf.debug : false
+    var debug = (this.config.debug) ? this.config.debug : false
     if (debug == true) log = _log
-    if (this.conf.display) {
-      this.wantedDisplay(true)
-      this.sendSocketNotification("PRESENCE", true)
-    }
-    log("Sensor Active: pin " + this.conf.pin)
-    this.pirConf.gpio = this.conf.pin
-    this.running = true
 
-    process.on('exit', (code) => {
-      if (this.conf.display) this.wantedDisplay(true)
-      console.log('[NewPIR] ByeBye !')
-      console.log('[NewPIR] @bugsounet')
-    });
+    this.NewPIR()
     console.log("[NewPIR] Initialize Complete Version:", require('./package.json').version)
   },
 
   socketNotificationReceived: function (notification, payload) {
     switch(notification) {
       case "INIT":
-        this.conf = payload
+        this.config = payload
         this.initialize()
         break
-      case "START":
-        this.pirDetect()
-        break
-      case "TIMER_EXPIRED":
-        this.running = false
-        this.sendSocketNotification("PRESENCE", false)
-        if (this.conf.display) this.wantedDisplay(false)
-        break
       case "WAKEUP":
-        this.running = true
-        this.sendSocketNotification("PRESENCE", true)
-        if (this.conf.display) this.wantedDisplay(true)
-        log("Wake Up Detected.")
+        this.screen.wakeup()
+        break
+      case "FORCE_END":
+        this.screen.forceEnd()
+        break
+      case "LOCK":
+        this.screen.lock()
+        break
+      case "UNLOCK":
+        this.screen.unlock()
         break
     }
   },
 
-  pirDetect: function() {
-    this.pir = new Pir(this.pirConf, this.pirCallback, this.conf.debug)
-    this.pir.start()
-  },
-
-  pirCallback: function (status, err) {
-    if (status == "PIR_DETECTED") {
-      log("Presence detected !")
-      this.sendSocketNotification("RESET_COUNTER")
-      this.wantedDisplay(true)
-      if (!this.running) {
-        this.sendSocketNotification("PRESENCE", true)
-        this.running = true
-      }
+  NewPIR: function () {
+    var callbacks= {
+      "sendSocketNotification": (noti, params) => {
+        this.sendSocketNotification(noti, params)
+        log("Callback Notification:", noti,params)
+      },
+      "screen": (param) => {
+        if (this.screen && param == "WAKEUP") this.screen.wakeup()
+      },
+      "pir": (param) => {
+        if (this.screen && this.pir && param == "PIR_DETECTED") this.screen.wakeup()
+      },
+      "governor": (param) => {
+        if (this.governor && param == "GOVERNOR_SLEEPING") this.governor.sleeping()
+        if (this.governor && param == "GOVERNOR_WORKING") this.governor.working()
+      },
     }
-    if (err) console.log("[NewPIR] " + err)
-  },
+    this.screen = new Screen(this.config.screen, callbacks.sendSocketNotification, this.config.debug, callbacks.sendSocketNotification, callbacks.governor)
+    this.screen.activate()
 
-  wantedDisplay: function(wanted) {
-    exec("/usr/bin/vcgencmd display_power", (err, stdout, stderr)=> {
-      if (err == null) {
-        var displaySh = stdout.trim()
-        var actual = Boolean(Number(displaySh.substr(displaySh.length -1)))
-        this.resultDisplay(actual,wanted)
-      }
-      else console.log("[NewPIR] Display: " + err)
-    })
-  },
-
-  resultDisplay: function(actual,wanted) {
-    log("Display -- Actual: " + actual + " - Wanted: " + wanted)
-    if (actual && !wanted) this.setDisplay(false)
-    if (!actual && wanted) this.setDisplay(true)
-  },
-
-  setDisplay: function(set) {
-    if (set)  exec("/usr/bin/vcgencmd display_power 1")
-    else exec("/usr/bin/vcgencmd display_power 0")
-    log("Display " + (set ? "ON." : "OFF."))
+    if (this.config.pir.usePir) {
+      this.pir = new Pir(this.config.pir, callbacks.pir, this.config.debug)
+      this.pir.start()
+    }
+    if (this.config.governor.useGovernor) {
+      this.governor = new Governor(this.config.governor, null, this.config.debug)
+      this.governor.start()
+    }
   }
 });
